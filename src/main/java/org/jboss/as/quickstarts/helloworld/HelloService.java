732018496c209faef9d1665811e6bc1e8aca9808
wenxingwang <wenxing.a.wang@capgemini.com>
2017-07-30 17:33:58 +0100

    3.0.2.Final-redhat-15 to 3.0.2.Final

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello " + name + " V1.0 !";
+        return "Hello " + name + " V2.0 !";
     }
 
 }
